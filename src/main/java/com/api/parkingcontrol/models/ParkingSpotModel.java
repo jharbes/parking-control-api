70181efebfa942d247781fad8d7215e0b56c81e9
Jorge Harbes <jharbes@hotmail.com>
2023-01-27 15:04:21 -0300

    Update ParkingSpotModel.java

--- src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -2,6 +2,7 @@ package com.api.parkingcontrol.models;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -46,4 +47,120 @@ public class ParkingSpotModel implements Serializable {
 
 	@Column(nullable = false, length = 30)
 	private String block;
+	
+	public ParkingSpotModel() {
+	}
+
+	public ParkingSpotModel(UUID id, String parkingSpotNumber, String licensePlateCar, String brandCar, String modelCar,
+			String colorCar, LocalDateTime registrationDate, String responsibleName, String apartment, String block) {
+		super();
+		this.id = id;
+		this.parkingSpotNumber = parkingSpotNumber;
+		this.licensePlateCar = licensePlateCar;
+		this.brandCar = brandCar;
+		this.modelCar = modelCar;
+		this.colorCar = colorCar;
+		this.registrationDate = registrationDate;
+		this.responsibleName = responsibleName;
+		this.apartment = apartment;
+		this.block = block;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getParkingSpotNumber() {
+		return parkingSpotNumber;
+	}
+
+	public void setParkingSpotNumber(String parkingSpotNumber) {
+		this.parkingSpotNumber = parkingSpotNumber;
+	}
+
+	public String getLicensePlateCar() {
+		return licensePlateCar;
+	}
+
+	public void setLicensePlateCar(String licensePlateCar) {
+		this.licensePlateCar = licensePlateCar;
+	}
+
+	public String getBrandCar() {
+		return brandCar;
+	}
+
+	public void setBrandCar(String brandCar) {
+		this.brandCar = brandCar;
+	}
+
+	public String getModelCar() {
+		return modelCar;
+	}
+
+	public void setModelCar(String modelCar) {
+		this.modelCar = modelCar;
+	}
+
+	public String getColorCar() {
+		return colorCar;
+	}
+
+	public void setColorCar(String colorCar) {
+		this.colorCar = colorCar;
+	}
+
+	public LocalDateTime getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDateTime registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public String getResponsibleName() {
+		return responsibleName;
+	}
+
+	public void setResponsibleName(String responsibleName) {
+		this.responsibleName = responsibleName;
+	}
+
+	public String getApartment() {
+		return apartment;
+	}
+
+	public void setApartment(String apartment) {
+		this.apartment = apartment;
+	}
+
+	public String getBlock() {
+		return block;
+	}
+
+	public void setBlock(String block) {
+		this.block = block;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ParkingSpotModel other = (ParkingSpotModel) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 }
