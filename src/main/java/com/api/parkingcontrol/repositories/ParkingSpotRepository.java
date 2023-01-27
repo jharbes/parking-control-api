eae804fc138ec40a6dbdec84b3ccb672809ff074
Jorge Harbes <jharbes@hotmail.com>
2023-01-27 15:16:47 -0300

    Create ParkingSpotRepository.java

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,16 @@
+package com.api.parkingcontrol.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+// JpaRepository possui varios metodos prontos pra serem utilizados pra transacoes com banco de dados, 
+// como por exemplo buscar uma listagem de algum recurso, buscar um recurso unico, atualizar, 
+// deletar entradas no banco de dados
+@Repository // nao obrigatoria a notacao @Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+}
