b50ad3331a072493339f971f4725ee7a00b61eb1
Jorge Harbes <jharbes@hotmail.com>
2023-01-30 15:00:03 -0300

    implementacao do metodo post pra salvar dados no banco de dados ok

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,10 +1,22 @@
 package com.api.parkingcontrol.controllers;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.parkingcontrol.dtos.ParkingSpotDto;
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -15,5 +27,25 @@ public class ParkingSpotController {
 
 	public ParkingSpotController(ParkingSpotService parkingSpotService) {
 		this.parkingSpotService = parkingSpotService;
+	}
+
+	// O bean @RequestBody é para que se receba os dados de parkingSpotDto no
+	// formato json
+	// O bean @Valid é importante pq sem ele nao serão feitas as validacoes
+	// presentes na classe ParkingSpotDto para os atributos como por exemplo
+	// @NotBlank e @Size
+	@PostMapping
+	public ResponseEntity<Object> saveParkingSlot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+		// o var declara a variavel sem que seja necessario especificar o tipo, ele
+		// mesmo atribui
+		var parkingSpotModel = new ParkingSpotModel();
+
+		// abaixo o metodo converte os dados da classe parkingSpotDto para
+		// parkingSpotModel, ou seja o objeto vazio parkingSpotModel recebera as
+		// informacoes do objeto parkingSpotDto
+		BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
 	}
 }
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -2,15 +2,27 @@ package com.api.parkingcontrol.services;
 
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
-	
+
 	// injecao de dependencia de ParkingSpotRepository em ParkingSpotService
 	final ParkingSpotRepository parkingSpotRepository;
-	
+
 	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
 		this.parkingSpotRepository = parkingSpotRepository;
+	}
+
+	// é interessante utilizar o bean transactional para que caso haja algum erro na
+	// transacao o proprio sistema desfaça a operação impedindo de se obter
+	// inconsistencias e/ou dados quebrados no banco de dados 
+	@Transactional
+	public Object save(ParkingSpotModel parkingSpotModel) {
+
+		return parkingSpotRepository.save(parkingSpotModel);
 	}
 }
